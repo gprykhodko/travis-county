1227ab2d3ff1c4839600f3b8838078bfc838c69d
Gennadii Prykhodko <66811202+gprykhodko@users.noreply.github.com>
2022-07-25 15:16:31 -0500

    Update script.js
    Add report button to pop up.

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
 require([
-    "esri/config",
-    "esri/WebMap",
-    "esri/views/MapView",
-    "esri/widgets/ScaleBar",
-    "esri/widgets/Legend",
-    "esri/Graphic",
-    "esri/rest/print",
-    "esri/rest/support/PrintTemplate",
-    "esri/rest/support/PrintParameters"
+  "esri/config",
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/widgets/ScaleBar",
+  "esri/widgets/Legend",
+  "esri/Graphic",
+  "esri/rest/print",
+  "esri/rest/support/PrintTemplate",
+  "esri/rest/support/PrintParameters"
 
-  ], (esriConfig, WebMap, MapView, ScaleBar, Legend, Graphic, print, PrintTemplate, PrintParameters) => {
+], (esriConfig, WebMap, MapView, ScaleBar, Legend, Graphic, print, PrintTemplate, PrintParameters) => {
+
+
+  esriConfig.portalUrl = "https://devportal.halff.com/portaldev";
+  
+  const webmap = new WebMap({
+    portalItem: {
+      id: "6927fdc1d10b4d61840f129820c5fa12"
+    }
+  });
+
+  const generateReport = {
+    title: "Generate Report",
+    id: "generate-report",
+    image: "http://static.arcgis.com/images/Symbols/PeoplePlaces/Information.png",
+    //className: "esri-icon-documentation"
+  };
+
+  const view = new MapView({
+    map: webmap,
+    container: "viewDiv",
+    popup: {
+      alignment: "top-right",
+      actions: [generateReport]
+    }
+  });
 
   
-    esriConfig.portalUrl = "https://devportal.halff.com/portaldev";
-    
-    const webmap = new WebMap({
-      portalItem: {
-        id: "6927fdc1d10b4d61840f129820c5fa12"
-      }
-    });
-    const view = new MapView({
-      map: webmap,
-      container: "viewDiv",
-      popup: {
-        alignment: "top-right"
-      }
-    });
 
-// url to the print service
+//url to the print service
 const url = "https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task";
 
-    const template = new PrintTemplate({
+const printTemplate = new PrintTemplate({
   format: "jpg",
   exportOptions: {
     dpi: 300
   },
   layout: "a4-portrait",
   layoutOptions: {
-    //titleText: "Flood Depth Report",
+  titleText: "    ",
     authorText: "Halff."
   }
 });
 
 const params = new PrintParameters({
-  view: view,
-  template: template
+   view: view,
+   template: printTemplate
 });
 
 
@@ -56,73 +67,115 @@ view.ui.add(printBtn, "top-right");
 
 printBtn.addEventListener("click", () => {
 
-    let popUpTitle = document.querySelector(".esri-popup__header-container--button").innerHTML
-    let popUpContents = document.querySelector(".esri-popup__content").innerHTML
-    let brandingLogo = '<br><center> <img src="https://www.halff.com/wp-content/themes/mastertheme-v3-halff/images/main-logo.png" height="40"> </center><br>'; // <h1 style="color:#0098db;">Report Description</h1, to add a logo insert <img src="https://www.halff.com/wp-content/themes/mastertheme-v3-halff/images/main-logo.png" height="40">
-    let divText = brandingLogo + popUpTitle + popUpContents + '<link rel="stylesheet" href="https://js.arcgis.com/4.21/esri/themes/light/main.css" />' 
-    const executePrint = print.execute(url, params)
-    .then(result => {
-      return result.url
-    }); 
-    
-    const printMap = (flood_info) => {
+  let popUpTitle = document.querySelector(".esri-popup__header-container--button").innerHTML
+  let popUpContents = document.querySelector(".esri-popup__content").innerHTML
+  let brandingLogo = '<br><center> <img src="https://www.halff.com/wp-content/themes/mastertheme-v3-halff/images/main-logo.png" height="40"> </center><br>'; // <h1 style="color:#0098db;">Report Description</h1, to add a logo insert <img src="https://www.halff.com/wp-content/themes/mastertheme-v3-halff/images/main-logo.png" height="40">
+  let divText = brandingLogo + popUpTitle + popUpContents + '<link rel="stylesheet" href="https://js.arcgis.com/4.21/esri/themes/light/main.css" />' 
+  const executePrint = print.execute(url, params)
+  .then(result => {
+    return result.url
+  }); 
+  
+  const printMap = (flood_info) => {
 
-        executePrint.then((m) => {
-        let map = `<center> <img src="${m}" height="800"> </center>`
-        let myWindow = window.open('', '', '');
-        let doc = myWindow.document;
-        doc.open();
-        doc.write(flood_info + map); 
-        doc.close();
-            setTimeout(function() { 
-            myWindow.print(); 
-             }, 1000);
-        
-    });
+      executePrint.then((m) => {
+      let map = `<center> <img src="${m}" height="800"> </center>`
+      let myWindow = window.open('', '', '');
+      let doc = myWindow.document;
+      doc.open();
+      doc.write(flood_info + map); 
+      doc.close();
+          setTimeout(function() { 
+          myWindow.print(); 
+           }, 1000);
       
-    };  
+  });
+    
+  };  
 
-     
-     printMap(divText);
-     
+   
+   printMap(divText);
+   
 
 });
 
+view.popup.on("trigger-action", (event) => {
 
-    view.on("click", function(event){
+  if (event.action.id === "generate-report") {
+    let popUpTitle = document.querySelector(".esri-popup__header-container--button").innerHTML
+  let popUpContents = document.querySelector(".esri-popup__content").innerHTML
+  let brandingLogo = '<br><center> <img src="https://www.halff.com/wp-content/themes/mastertheme-v3-halff/images/main-logo.png" height="40"> </center><br>'; // <h1 style="color:#0098db;">Report Description</h1, to add a logo insert <img src="https://www.halff.com/wp-content/themes/mastertheme-v3-halff/images/main-logo.png" height="40">
+  let divText = brandingLogo + popUpTitle + popUpContents + '<link rel="stylesheet" href="https://js.arcgis.com/4.21/esri/themes/light/main.css" />' 
+  const executePrint = print.execute(url, params)
+  .then(result => {
+    return result.url
+  }); 
+
+  const printMap = (flood_info) => {
+
+    executePrint.then((m) => {
+    let map = `<center> <img src="${m}" height="800"> </center>`
+    let myWindow = window.open('', '', '');
+    let doc = myWindow.document;
+    doc.open();
+    doc.write(flood_info + map); 
+    doc.close();
+        setTimeout(function() { 
+        myWindow.print(); 
+         }, 1000);
+    
+});
+  
+};  
+
+ 
+ printMap(divText);
+
+  
+
+  }
+});
+
+
+  view.on("click", event => {
+
         createGraphic(event.mapPoint.latitude, event.mapPoint.longitude);
+        //console.log(event.x, event.mapPoint.latitude);
+  
+    });
+
+    /*************************
+     * Create a point graphic
+     *************************/
+    function createGraphic(lat, long) {
+      // First create a point geometry 
+      let point = {
+        type: "point", // autocasts as new Point()
+        longitude: long,
+        latitude: lat
+      };
+
+      // Create a symbol for drawing the point
+        let markerSymbol = {
+          type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
+          url: "https://static.arcgis.com/images/Symbols/Basic/RedStickpin.png",
+          width: "44px",
+          height: "44px",
+          yoffset: "19px"
+        };
+      
+
+      // Create a graphic and add the geometry and symbol to it
+      let pointGraphic = new Graphic({
+        geometry: point,
+        symbol: markerSymbol
       });
 
-      /*************************
-       * Create a point graphic
-       *************************/
-      function createGraphic(lat, long){
-        // First create a point geometry 
-        var point = {
-          type: "point", // autocasts as new Point()
-          longitude: long,
-          latitude: lat
-        };
+      // Add the graphics to the view's graphics layer
+      view.graphics.removeAll();
+      view.graphics.add(pointGraphic);
+      
+    }
 
-        // Create a symbol for drawing the point
-          let markerSymbol = {
-            type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
-            url: "https://static.arcgis.com/images/Symbols/Basic/RedStickpin.png",
-            width: "44px",
-            height: "44px",
-            yoffset: "19px"
-          };
-        
-
-        // Create a graphic and add the geometry and symbol to it
-        var pointGraphic = new Graphic({
-          geometry: point,
-          symbol: markerSymbol
-        });
-
-        // Add the graphics to the view's graphics layer
-        view.graphics.add(pointGraphic);
-      }
-
-    
-  });
+  
+});
